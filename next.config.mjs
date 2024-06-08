6ebf3dfcdbf126ce9869c825eac539fb853db073
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-06-08 13:11:45 -0500

    chore: Update deploy workflow to use Node.js 18-alpine image and build project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+const buildDirectory = process.env.BUILD_DIRECTORY || '';
+
+const nextConfig = {
+	distDir: isProd ? `${buildDirectory}` : undefined,
+};
 
 export default nextConfig;
