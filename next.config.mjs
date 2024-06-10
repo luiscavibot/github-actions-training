11576856c14d904aafe1a583a01383f959d4e250
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-06-10 11:38:21 -0500

    chore: Update build directory to 'build-in-local' for local development

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
-const buildDirectory = process.env.BUILD_DIRECTORY;
+const buildDirectory = process.env.BUILD_DIRECTORY || 'build-in-local';
+console.log(`Build directory>>> ${buildDirectory}`);
 
 const nextConfig = {
-	distDir: isProd ? `${buildDirectory}` : undefined,
+	distDir: isProd ? `${buildDirectory}` : '.next',
 };
 
 export default nextConfig;
