de874e9d7e27870482a942c867fa94c0bc9735c2
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-06-10 12:14:42 -0500

    chore: Update next.config.mjs to log NODE_ENV variable

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 const buildDirectory = process.env.BUILD_DIRECTORY || 'build-in-local';
 console.log(`Build directory>>> ${buildDirectory}`);
+console.log(`process.env.NODE_ENV >>> ${process.env.NODE_ENV}`);
 
 const nextConfig = {
 	distDir: isProd ? `${buildDirectory}` : '.next',
