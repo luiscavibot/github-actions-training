34cc3cb7ae524cfb896bce5f6cf292bc79cc4ce3
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-06-10 12:52:38 -0500

    chore: Update Dockerfile to include build directory argument and environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
-const buildDirectory = process.env.BUILD_DIRECTORY || 'build-in-local';
+const buildDirectory = process.env.BUILD_DIRECTORY || 'home';
 console.log(`Build directory>>> ${buildDirectory}`);
 console.log(`process.env.NODE_ENV >>> ${process.env.NODE_ENV}`);
 
 const nextConfig = {
-	distDir: isProd ? `${buildDirectory}` : '.next',
+	distDir: isProd ? `${buildDirectory}` : undefined,
 };
 
 export default nextConfig;
