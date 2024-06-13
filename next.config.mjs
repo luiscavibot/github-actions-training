f021472f16bc31de26f5bd4a4e75f3a2cd26b64f
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-06-13 10:44:20 -0500

    chore: Update S3 bucket cleanup in deploy-app.yml and distDir configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,9 @@
 
 const isProd = process.env.NODE_ENV === 'production';
 const buildDirectory = process.env.BUILD_DIRECTORY || 'home';
-console.log(`Build directory>>> ${buildDirectory}`);
-console.log(`process.env.NODE_ENV >>> ${process.env.NODE_ENV}`);
 
 const nextConfig = {
-	distDir: isProd ? `${buildDirectory}` : undefined,
+	distDir: isProd ? `${buildDirectory}` : 'home',
 	assetPrefix: `https://d13jgt8gwoao83.cloudfront.net/${buildDirectory}`,
 };
 
