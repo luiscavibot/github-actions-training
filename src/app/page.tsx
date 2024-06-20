3b8e645f9ddd6f530203f456c288cde8f4de0548
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-06-20 16:16:50 -0500

    chore: Update page title in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
 			<div className="mb-32 grid text-center">
 				<h1 className="text-3xl">
-					STORE FRONT Continuous Deployment -TEST 3{' '}
+					STORE FRONT Continuous Deployment -TEST 4{' '}
 				</h1>
 			</div>
 		</main>
